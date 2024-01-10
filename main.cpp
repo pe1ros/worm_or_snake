e48861a9989317573df0048f9ec73b848710229c
Ilya Nikolaevich <im.clean@yandex.ru>
2024-01-10 18:19:29 +0300

    Add game scope, add working process after snake stay on eat place/reinit eat after it

--- main.cpp
+++ main.cpp
@@ -30,28 +30,21 @@ struct SnakePart {
   SnakePart* next;
 };
 
-struct Apple {
+struct Eat {
   int x_pos_;
   int y_pos_;
   bool initialized_;
 };
 
-void GenerateEat(Apple& apple, SnakePart& snake, int size_rows) {
-  if (!apple.initialized_) {
-    apple.x_pos_ = std::rand() % size_rows;
-    apple.y_pos_ = std::rand() % size_rows;
-    do {
-      apple.y_pos_ = std::rand() % size_rows;
-      apple.x_pos_ = std::rand() % size_rows;
-    } while (snake.x_pos_ == apple.x_pos_ || snake.y_pos_ == apple.y_pos_);
-    apple.initialized_ = true;
-  }
-}
-
 class Game {
  public:
   Game(int size_rows, bool isActive)
-      : size_rows_(size_rows), game_is_active_(isActive) {}
+      : size_rows_(size_rows), game_is_active_(isActive), scope_(0) {
+    snake_head_->x_pos_ = std::rand() % size_rows_;
+    snake_head_->y_pos_ = std::rand() % size_rows_;
+    snake_head_->next = nullptr;
+    snake_head_->prev = nullptr;
+  }
 
   ~Game() {
     for (int i = 0; i < size_rows_; i++) {
@@ -79,73 +72,97 @@ class Game {
       }
     }
 
-    snake_.x_pos_ = std::rand() % size_rows_;
-    snake_.y_pos_ = std::rand() % size_rows_;
-    snake_.next = nullptr;
-    snake_.prev = nullptr;
     std::thread listener(keyListener);
     while (game_is_active_) {
-      GenerateEat(apple_, snake_, size_rows_);
       Move();
+      if (!eat_.initialized_) {
+        GenerateEat();
+      }
       Draw();
+      sleep(1);
     }
     listener.join();
   }
 
   void Move() {
-    sleep(1);
     char value = keyPressed.load();
+    // int snake_prev_x = snake_head_->x_pos_;
+    // int snake_prev_y = snake_head_->y_pos_;
     switch (value) {
       case 'w':
-        --snake_.x_pos_;
+        snake_head_->x_pos_ <= 0 ? snake_head_->x_pos_ = size_rows_ - 1
+                                 : --snake_head_->x_pos_;
         break;
       case 's':
-        ++snake_.x_pos_;
+        snake_head_->x_pos_ >= size_rows_ - 1 ? snake_head_->x_pos_ = 0
+                                              : ++snake_head_->x_pos_;
         break;
       case 'a':
-        --snake_.y_pos_;
+        snake_head_->y_pos_ <= 0 ? snake_head_->y_pos_ = size_rows_ - 1
+                                 : --snake_head_->y_pos_;
         break;
       case 'd':
-        ++snake_.y_pos_;
+        snake_head_->y_pos_ >= size_rows_ - 1 ? snake_head_->y_pos_ = 0
+                                              : ++snake_head_->y_pos_;
         break;
     }
-
-    if (snake_.x_pos_ == size_rows_) {
-      snake_.x_pos_ = 0;
-      return;
-    } else if (snake_.x_pos_ < 0) {
-      snake_.x_pos_ = size_rows_ - 1;
-      return;
-    } else if (snake_.y_pos_ == size_rows_) {
-      snake_.y_pos_ = 0;
-      return;
-    } else if (snake_.y_pos_ < 0) {
-      snake_.y_pos_ = size_rows_ - 1;
-      return;
+    if (snake_head_->x_pos_ == eat_.x_pos_ &&
+        snake_head_->y_pos_ == eat_.y_pos_) {
+      // snake_head_->next = new SnakePart;
+      // snake_head_->next->x_pos_ = eat_.x_pos_;
+      // snake_head_->next->y_pos_ = eat_.y_pos_;
+      eat_.initialized_ = false;
+      ++scope_;
     }
+    // if (length_ > 0) {
+    //   while (snake_body_ != NULL) {
+    //     snake_body_->next->x_pos_ = snake_prev_x;
+    //     snake_body_->next->y_pos_ = snake_prev_y;
+    //     snake_body_ = snake_body_->next;
+    //   }
+    // }
   }
 
   void Draw() {
     system("clear");
+    std::cout << "_____scope_____: " << scope_ << "\n";
+    for (int i = 0; i < size_rows_; i++) {
+      for (int j = 0; j < size_rows_; j++) {
+        ptr_polygon_[i][j] = '.';
+      }
+    }
+    ptr_polygon_[eat_.x_pos_][eat_.y_pos_] = '#';
+    ptr_polygon_[snake_head_->x_pos_][snake_head_->y_pos_] = '@';
+    // while (snake_head_->next != NULL) {
+    //   ptr_polygon_[snake_head_->next->x_pos_][snake_head_->next->y_pos_] =
+    //   '*'; snake_head_ = snake_head_->next;
+    // }
 
     for (int i = 0; i < size_rows_; i++) {
       for (int j = 0; j < size_rows_; j++) {
-        snake_.x_pos_ == i&& snake_.y_pos_ == j
-            ? std::cout << "@"
-                        << " "
-            : std::cout << ptr_polygon_[i][j] << " ";
+        std::cout << ptr_polygon_[i][j] << " ";
       }
       std::cout << "\n";
     }
-    ptr_polygon_[apple_.x_pos_][apple_.y_pos_] = '#';
+  }
+
+  void GenerateEat() {
+    do {
+      eat_.y_pos_ = std::rand() % size_rows_;
+      eat_.x_pos_ = std::rand() % size_rows_;
+    } while (snake_head_->x_pos_ == eat_.x_pos_ ||
+             snake_head_->y_pos_ == eat_.y_pos_);
+    eat_.initialized_ = true;
   }
 
  private:
   int size_rows_;
   char** ptr_polygon_;
-  SnakePart snake_;
-  Apple apple_;
+  SnakePart* snake_head_;
+  // TODO: подумать почему тут не работает инициализация через указатель!?!?
+  Eat eat_;
   bool game_is_active_;
+  int scope_;
 };
 
 int main() {
